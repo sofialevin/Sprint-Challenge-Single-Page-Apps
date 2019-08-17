e0efac950b5012e87aa3777f90b2c6642d24b5c9
sofialevin <slevin@M-4876.local>
2019-08-17 11:26:03 -0700

    Completed basic unstyled nav.

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Route } from 'react-router-dom';
+import WelcomePage from "./WelcomePage.js";
+import CharacterList from "./CharacterList.js";
+import LocationsList from "./LocationsList.js";
+import EpisodesList from "./EpisodesList"
 
 export default function AppRouter() {
     return (
-        <div></div>
+    <div>
+        <Route exact path="/" component={WelcomePage}/>
+        <Route path="/character" component={CharacterList}/>
+        <Route path="/location" component={LocationsList}/>
+        <Route path="/episode" component={EpisodesList}/>
+    </div>
     )
 }
--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -5,6 +5,19 @@ import { NavLink } from "react-router-dom";
 // TODO: Add missing tabs below
 export default function TabNav() {
     return (
-        <div></div>
+        <div>
+            <div>
+                <NavLink to="/">Home Page</NavLink>
+            </div>
+            <div>
+                <NavLink to="/character">Characters</NavLink>
+                </div>
+            <div>
+                <NavLink to="/location">Locations</NavLink>
+                </div>
+            <div>
+                <NavLink to="/episode">Episodes</NavLink>
+            </div>
+        </div>
     )
 };
